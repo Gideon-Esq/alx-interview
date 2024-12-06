3a9732b12a495bcb2c2608961af0cd9beffe2d46
Ayooluwa Gideon Oloyede <ayooluwaoloyedee71@gmail.com>
2024-12-06 20:14:18 +0000

    def island_perimeter(grid): returns the perimeter of the island described in grid

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
+"""
+Module: island_perimeter
+
+This module contains a function to calculate the perimeter of an island
+represented in a grid. The grid is a 2D list where:
+- 0 represents water
+- 1 represents land
+
+The `island_perimeter` function takes this grid as input and returns
+the total perimeter of the island(s).
+
+Usage:
+    grid = [
+        [0, 1, 0, 0],
+        [1, 1, 1, 0],
+        [0, 1, 0, 0],
+        [1, 1, 0, 0]
+    ]
+    print(island_perimeter(grid))  # Output: 16
+"""
 
 
 def island_perimeter(grid):
